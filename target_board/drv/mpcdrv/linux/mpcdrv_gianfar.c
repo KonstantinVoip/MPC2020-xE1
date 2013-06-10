2284fcc7ad1b99201e20b22b4ed9304e350c56fe
kosta_home <gasten_tss18@mail.ru>
2013-06-10 22:24:31 +0400

    добавил mpc gianfar

---
+++ target_board/drv/mpcdrv/linux/mpcdrv_gianfar.c
@@ -0,0 +1,171 @@
+/**********************************************************************************************************************
+*                                        (c) COPYRIGHT by OAO SUPERTEL.                                             *
+*                                               All rights reserved.                                                  *
+***********************************************************************************************************************
+* Module      : mpcdrv_gianfar.c
+*             
+* Description : Defenition of low level fubction.
+*
+* Author      : Konstantin Shiluaev
+*
+******************************************************************************
+******************************************************************************
+*
+* Module's Description Record:
+* ============================
+*
+* $ProjectRevision: 0.0.1 $
+* $Source: 
+* $State: Debug$
+* $Revision: 0.0.1 $ $Name: $
+* $Date: 2013/06/10 10:40:51 $
+* $Locker: $
+*
+* Module's revision history:
+* ==========================
+* --------- $Log: mpcdrv_gianfar.c $
+* --------- Initial revision
+******************************************************************************/
+/*-----------------------------------------------------------------------------
+GENERAL NOTES
+-----------------------------------------------------------------------------*/
+
+/*****************************************************************************/
+/*	COMMON CONFIGURATION						     */
+/*****************************************************************************/
+
+/*****************************************************************************/
+/*	INCLUDES							     */
+/*****************************************************************************/
+#include <linux/kernel.h>
+#include <linux/string.h>
+#include <linux/errno.h>
+#include <linux/unistd.h>
+#include <linux/slab.h>
+#include <linux/interrupt.h>
+#include <linux/init.h>
+#include <linux/delay.h>
+#include <linux/netdevice.h>
+#include <linux/etherdevice.h>
+#include <linux/skbuff.h>
+#include <linux/if_vlan.h>
+#include <linux/spinlock.h>
+#include <linux/mm.h>
+#include <linux/of_mdio.h>
+#include <linux/of_platform.h>
+#include <linux/ip.h>
+#include <linux/tcp.h>
+#include <linux/udp.h>
+#include <linux/in.h>
+#include <linux/inetdevice.h>
+
+#include <asm/io.h>
+#include <asm/irq.h>
+#include <asm/uaccess.h>
+#include <linux/module.h>
+#include <linux/dma-mapping.h>
+#include <linux/crc32.h>
+#include <linux/mii.h>
+#include <linux/phy.h>
+#include <linux/phy_fixed.h>
+#include <linux/of.h>
+#include <net/xfrm.h>
+#include <sysdev/fsl_soc.h>
+#ifdef CONFIG_GFAR_SW_PKT_STEERING
+#include <asm/fsl_msg.h>
+#endif
+
+#ifdef CONFIG_NET_GIANFAR_FP
+#include <linux/if_arp.h>
+#include <linux/netdevice.h>
+#include <net/route.h>
+#include <net/ip.h>
+#include <linux/jhash.h>
+#endif
+
+//#include "gianfar.h"
+#include "fsl_pq_mdio.h"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include "mpcdrveth_gianfar.h"
+
+
+
+/**************************************************************************************************
+Syntax:      	  SINT32 InitMpcGianfar()
+
+Remarks:		  Init function for module send Ipv4 Packet
+
+Return Value:	  Returns 1 on success and negative value on failure.
+
+ 				Value		 									Description
+				-------------------------------------------------------------------------------------
+				= 1												Success
+				=-1												Failure
+***************************************************************************************************/
+SINT32 InitEthipv4drv()
+{
+int cpu;
+
+
+return 1;
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ target_board/drv/mpcdrv/linux/mpcdrv_gianfar.h
@@ -0,0 +1,96 @@
+/**********************************************************************************************************************
+*                                        (c) COPYRIGHT by OAO SUPERTEL.                                             *
+*                                               All rights reserved.                                                  *
+***********************************************************************************************************************
+* Module      : mpcdrv_gianfar.h
+*
+* Description : Low Level MPC P2020 Ethernet Driver. 
+*
+* Author      : Konstantin Shiluaev
+*
+******************************************************************************
+******************************************************************************
+*
+* Module's Description Record:
+* ============================
+*
+* $ProjectRevision: 0.0.1 $
+* $Source: 
+* $State: Debug$
+* $Revision: 0.0.1 $ $Name: $
+* $Date: 2013/06/11 22:12:51 $
+* $Locker: $
+*
+* Module's revision history:
+* ==========================
+* --------- $Log: mpcdrv_gianfar.h $
+* --------- Initial revision
+******************************************************************************/
+
+/*-----------------------------------------------------------------------------
+ GENERAL NOTES
+-----------------------------------------------------------------------------*/
+#ifndef MPCDRVGIANFAR_H
+#define MPCDRVGIANFAR_H
+
+/*****************************************************************************/
+/*	COMMON CONFIGURATION						     */
+/*****************************************************************************/
+
+
+/*****************************************************************************/
+/*	INCLUDES							     */
+#include <linux/kernel.h>
+#include <linux/sched.h>
+#include <linux/string.h>
+#include <linux/errno.h>
+#include <linux/slab.h>
+#include <linux/interrupt.h>
+#include <linux/init.h>
+#include <linux/delay.h>
+#include <linux/netdevice.h>
+#include <linux/etherdevice.h>
+#include <linux/skbuff.h>
+#include <linux/spinlock.h>
+#include <linux/mm.h>
+#include <linux/mii.h>
+#include <linux/phy.h>
+
+#include <asm/io.h>
+#include <asm/irq.h>
+#include <asm/uaccess.h>
+#include <linux/module.h>
+#include <linux/crc32.h>
+#include <linux/workqueue.h>
+#include <linux/ethtool.h>
+
+#ifdef CONFIG_GIANFAR_L2SRAM
+#include <asm/fsl_85xx_cache_sram.h>
+#define ALIGNMENT 0x20
+#endif
+
+
+/*	INCLUDES							     */
+#include "/mnt/SHARE/MPC2020-xE1/target_board/app/mpcapp/INC/mpcUniDef.h"
+
+/*****************************************************************************/
+/*	PUBLIC MACROS							     */
+/*****************************************************************************/
+
+
+
+/*****************************************************************************/
+/*	PUBLIC DATA TYPES						     */
+/*****************************************************************************/
+
+
+/*****************************************************************************/
+/*	PUBLIC FUNCTION PROTOTYPES					     */
+/*****************************************************************************/
+
+
+/*****************************************************************************/
+/*	PUBLIC GLOBALS							     */
+/*****************************************************************************/
+#endif /* MPCDRVETHIPV4_H */
+
